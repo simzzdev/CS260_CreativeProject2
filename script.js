98cc41c21100c802b94c6e37fe578a5c1362f5ff
John Simmons <I99458@verisk.com>
2020-02-22 12:13:08 -0700

    Images and some styling

--- script.js
+++ script.js
@@ -6,6 +6,23 @@ let searchBar = document.getElementById("searchBar");
 let results = document.getElementById("results");
 let searchSuggestions = document.getElementById("searchSuggestions");
 
+Date.prototype.toNiceDateString = function() {
+	let month = "";
+	if (this.getMonth() == 0){month = "January"};
+	if (this.getMonth() == 1){month = "February"};
+	if (this.getMonth() == 2){month = "March"};
+	if (this.getMonth() == 3){month = "April"};
+	if (this.getMonth() == 4){month = "May"};
+	if (this.getMonth() == 5){month = "June"};
+	if (this.getMonth() == 6){month = "July"};
+	if (this.getMonth() == 7){month = "August"};
+	if (this.getMonth() == 8){month = "September"};
+	if (this.getMonth() == 9){month = "October"};
+	if (this.getMonth() == 10){month = "November"};
+	if (this.getMonth() == 11){month = "December"};
+	return month + " " + this.getDate() + ", " + this.getFullYear();
+}
+
 function searchPlayer(playerName) {
 	let webRequest = fetch(playerEndpoint + playerName);
 	let jsonRequest = webRequest.then(function (resp) {
@@ -16,16 +33,18 @@ function searchPlayer(playerName) {
 	});
 	jsonRequest.then(function(json) {
 		let totalHtml = "";
-		document.getElementById("playerName").innerText = json[0].Name + " " + json[0].Position_name;
+		document.getElementById("playerName").innerText = json[0].Name + ", " + json[0].Position_name;
 
 		for (let i = 0; i < json.length; i++) {
 			let crime = "<div class='crime' style='background-color:#" + json[i].Team_hex_color + "' >";
-			crime += "<p class='crimeDate'>" + json[i].Date + "</p>";
+			let date = new Date(json[i].Date);
+			crime += "<p class='crimeDate'>" + date.toNiceDateString() + "</p>";
 			crime += "<div class='teamDiv'>";
-			crime += "<img src='' alt= ' ' />";
+			let imgName = "images/teams/" + json[i].Team_preffered_name.toLowerCase().replace(" ", "-").replace(" ", "-") + ".png";
+			crime += "<img width=36 height=36 src=" + imgName + " alt= ' ' />";
 			crime += "<p class='teamName'>" + json[i].Team_preffered_name + "</p>";
 			crime += "</div>";
-			crime += "<p class='crimeDate'>" + json[i].Category + "</p>";
+			crime += "<p class='crimeCategory'>" + json[i].Category + "</p>";
 			crime += "<p class='crimeDescription'>" + json[i].Description + "</p>";
 			crime += "<p class='crimeOutcome'>" + json[i].Outcome + "</p>";
 			crime += "</div>";
